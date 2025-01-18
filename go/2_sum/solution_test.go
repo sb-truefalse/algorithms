4879ad19fdc9d66d5548fd4e29f026f9d797e370
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-01-18 18:21:01 +0300

    Fix go-test

--- go/2_sum/solution_test.go
+++ go/2_sum/solution_test.go
@@ -9,8 +9,8 @@ func TestTwoSum(t *testing.T) {
 	list := []int{1, 2, 3, 4, 5, 6, 7, 9, 10}
 	x := 5
 	got1, got2 := TwoSum(list, x)
-	if got1 == -1 && got2 == -1 {
-		t.Errorf("TwoSum(%#v, %d) = %d %d; want 2 3", list, x, got1, got2)
+	if got1 != 0 && got2 != 3 {
+		t.Errorf("TwoSum(%#v, %d) = %d %d; want 0 3", list, x, got1, got2)
 	}
 }
 
@@ -18,8 +18,8 @@ func TestTwoSumWith2Pointers(t *testing.T) {
 	list := []int{1, 2, 3, 4, 5, 6, 7, 9, 10}
 	x := 5
 	got1, got2 := TwoSumWith2Pointers(list, x)
-	if got1 == -1 && got2 == -1 {
-		t.Errorf("TwoSumWith2Pointers(%#v, %d) = %d %d; want 2 3", list, x, got1, got2)
+	if got1 != 0 && got2 != 3 {
+		t.Errorf("TwoSumWith2Pointers(%#v, %d) = %d %d; want 0 3", list, x, got1, got2)
 	}
 }
 
@@ -27,7 +27,7 @@ func TestTwoSumWithHash(t *testing.T) {
 	list := []int{1, 2, 3, 4, 5, 6, 7, 9, 10}
 	x := 5
 	got1, got2 := TwoSumWithHash(list, x)
-	if got1 == -1 && got2 == -1 {
-		t.Errorf("TwoSumWithHash(%#v, %d) = %d %d; want 2 3", list, x, got1, got2)
+	if got1 != 0 && got2 != 3 {
+		t.Errorf("TwoSumWithHash(%#v, %d) = %d %d; want 0 3", list, x, got1, got2)
 	}
 }
